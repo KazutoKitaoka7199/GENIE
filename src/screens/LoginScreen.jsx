05bb50424ad94af9c469d4d1e6bc779e3eac08b4
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-02-22 23:59:51 +0900

    create LoginScreen

---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  View, Text, TextInput, StyleSheet,
+} from 'react-native';
+import AppBar from '../components/AppBar';
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View>
+        <Text>Login</Text>
+        <TextInput value="Email Address" />
+        <TextInput value="Password" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
