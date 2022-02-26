dfec6ea12e424d54485fc98c510ddf70a674e232
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-02-26 17:42:34 +0900

    immplement logout

---
+++ src/components/LogOutButton.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Text, StyleSheet, TouchableOpacity, Alert,
+} from 'react-native';
+import firebase from 'firebase';
+import { useNavigation } from '@react-navigation/native';
+
+export default function LogOutButton() {
+  // React Hooksは直下で定義
+  const navigation = useNavigation();
+  function handPress() {
+    firebase.auth().signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'Login' }],
+        });
+      })
+      .catch(() => {
+        Alert.alert('ログアウトに失敗しました');
+      });
+  }
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      // eslint-disable-next-line react/jsx-no-bind
+      onPress={handPress}
+    >
+      <Text style={styles.label}>ログアウト</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  label: {
+    fontSize: 14,
+    color: 'rgba(255,255,255,0.7)',
+  },
+});
